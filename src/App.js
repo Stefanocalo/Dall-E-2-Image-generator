f8efe2853d1baeabbd83dc8118b980faaf711fba
Stefano Calo <stefano.cal92@gmail.com>
2022-12-02 14:57:48 +0100

    Add openai api call

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
-import {React} from 'react';
+import {React, useSyncExternalStore} from 'react';
 import { useState } from 'react';
 import './App.css';
+import { Configuration, OpenAIApi } from 'openai';
+
 
 function App () {
 
-  // Declaring state
+  // ---- Declaring state ----
+
+    // Prompt state variable
   const [prompt, setPrompt] = useState('');
 
-  const generateImage = async () => {};
+    //API call result state variable
+  
+  const [result, setResult] = useState('')
+
+  // -----------------------------------------
+
+
+  // ---- Declare configuration variable ----
+
+  const configuration = new Configuration({
+    apiKey: process.env.REACT_APP_DALLE_API_KEY,
+  });
+
+  const openai = new OpenAIApi(configuration);
+  
+  // -----------------------------------------
+
+  const generateImage = async () => {
+    await openai.createImage({
+      prompt: prompt,
+      n: 1,
+      size: "512x512",
+    });
+    setResult(process.data.data[0].url);
+  };
 
   const handleChange = ({target}) => {
     setPrompt(target.value);
@@ -24,6 +52,15 @@ function App () {
       <button
         onClick={generateImage}
       >Generate Image</button>
+      <div className='imageContainer'>
+        {
+        result.length > 0 ? (
+            <img className="result-image" src={result} alt="result" />
+          ) : (
+            <></>
+        )}
+        
+      </div>
     </div>
   )
 
